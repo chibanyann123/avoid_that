6a75456ab245592ab25a244a62c1b0a3cbec127b
chibanyann123 <c1339828@st.kanazawa-it.ac.jp>
2023-10-11 19:23:35 +0900

    second

--- src/avoid_that_master.py
+++ src/avoid_that_master.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-#-*-coding:utr-8 -*-
+#-*-coding:utf-8 -*-
 
 import rospy
 import smach
 from happymimi_navigation.srv import NaviLocation
 from enter_room.srv import EnterRoom
 
-class Door(samch.State):
+class Door(smach.State):
     def __init__(self):
-        smach.State __init__(self,outcomes=['Door_end'])
+        smach.State. __init__(self,outcomes=['Door_end'])
         self.enter_room = rospy.ServiceProxy('/enter_room_server',EnterRoom)
     
     def execute(self):
@@ -16,15 +16,14 @@ class Door(samch.State):
         return 'Door_end'
     
 
-class navi(samch.State):
+class Navi(smach.State):
     def __init__(self):
-        smach.State __init__(self,outcomes=['Navi_end'])
+        smach.State. __init__(self,outcomes=['Navi_end'])
         self.nav = rospy.ServiceProxy('/navi_location_server',NaviLocation)
     
     def execute(self):
         self.navi.nav = ('living')
         return 'Navi_end'
-
 
 if __name__=='__main__':
    sm  = smach.StateMachine(outcomes = ['fmm_end'])
