34606efbeee52426470d2ad27b577c57d49bc402
chibanyann123 <137049403+chibanyann123@users.noreply.github.com>
2023-10-19 15:17:23 +0900

    Update avoid_that_master.py

--- src/avoid_that_master.py
+++ src/avoid_that_master.py
@@ -11,7 +11,7 @@ class Door(smach.State):
         smach.State. __init__(self,outcomes=['Door_end'])
         self.enter_room = rospy.ServiceProxy('/enter_room_server',EnterRoom)
     
-    def execute(self):
+    def execute(self._):
         self.enter_room(0.5,0.3)
         return 'Door_end'
     
@@ -21,7 +21,7 @@ class Navi(smach.State):
         smach.State. __init__(self,outcomes=['Navi_end'])
         self.nav = rospy.ServiceProxy('/navi_location_server',NaviLocation)
     
-    def execute(self):
+    def execute(self._):
         self.navi.nav = ('living')
         return 'Navi_end'
 
@@ -30,12 +30,12 @@ if __name__=='__main__':
    sm.userdata.navi_num = 0
 
    with sm:
-       smach.StateMachine.add("Door",
+       smach.StateMachine.add("DOOR",
                               Door(),
-                              transitions = {"Door_end":"Navi"})
+                              transitions = {"Door_end":"NAVI"})
        
-       smach.StateMachine.add("Navi",
+       smach.StateMachine.add("NAVI",
                               Navi(),
                               transitions = {"Navi_end":"fmm_end"})
        
-outcoms = sm.execute()  
+outcome = sm.execute()  
