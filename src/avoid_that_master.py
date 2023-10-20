f1cf7292edecd74aba75f37cc01fa2dcc25835ec
chibanyann123 <137049403+chibanyann123@users.noreply.github.com>
2023-10-20 17:34:06 +0900

    Update avoid_that_master.py

--- src/avoid_that_master.py
+++ src/avoid_that_master.py
@@ -22,7 +22,7 @@ class Navi(smach.State):
         self.nav = rospy.ServiceProxy('/navi_location_server',NaviLocation)
     
     def execute(self,_):
-        self.navi = ('living')
+        self.navi = ('shelf')
         return 'Navi_end'
 
 if __name__=='__main__':
